006b0caccd59935a3d05aea2308e25bedaa831b5
Zenzicubic <58337545+zenzicubic@users.noreply.github.com>
2025-05-22 23:50:14 -0400

    Update license info to match Wikipedia

--- src/components/copyright-notice/CopyrightNotice.js
+++ src/components/copyright-notice/CopyrightNotice.js
@@ -11,11 +11,10 @@ import './copyright-notice.css';
 function CopyrightNotice() {
     return (<div className="copyright-notice">
         <p>All images and articles licensed under&nbsp;
-            <a href="http://creativecommons.org/licenses/by-nc/4.0" target="_blank" rel="license noopener noreferrer">
-                CC BY-NC 4.0
+            <a href="http://creativecommons.org/licenses/by/4.0" target="_blank" rel="license noopener noreferrer">
+                CC BY 4.0
                     <img className="cc-icon" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg" alt="" id="icon-0" />
                     <img className="cc-icon" src="https://mirrors.creativecommons.org/presskit/icons/by.svg" alt=""/>
-                    <img className="cc-icon" src="https://mirrors.creativecommons.org/presskit/icons/nc.svg" alt=""/>
                     </a>.
                 All source code for this website licensed under the <a href="https://github.com/zenzicubic/zenzicubic-site/blob/main/LICENSE" target="_blank" rel="noopener noreferrer">MIT License</a>.
         </p>
